c135436dc196e4c17f3f09b1bb941e6192e2cef7
Piyush2k23 <piyush23pr2000@gmail.com>
2024-02-23 15:56:57 +0530

    setup mind swap project using nextjs

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Mind Swap",
+  description: "MindExchange is an innovative chat-based platform that facilitates seamless interaction between users and an advanced AI model.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
